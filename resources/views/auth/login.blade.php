331b336cccac0f0b26d0b1010c02406a504c8eff
najjaf <najaf2961@gmail.com>
2025-08-20 10:31:35 +0500

    fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,8 +19,22 @@
                             <a href="{{ url('/') }}" class="app-brand-link">
                                 <span class="app-brand-logo demo">
                                     <span class="text-primary">
-                                        <!-- Your SVG Logo Here -->
-                                        <!-- ... -->
+                                        <!-- SVG logo -->
+                                        <svg width="32" height="22" viewBox="0 0 32 22" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                                d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
+                                                fill="currentColor" />
+                                            <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
+                                                d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
+                                                fill="#161616" />
+                                            <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
+                                                d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
+                                                fill="#161616" />
+                                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                                d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
+                                                fill="currentColor" />
+                                        </svg>
                                     </span>
                                 </span>
                                 <span class="app-brand-text demo text-heading fw-bold">HiveHomes</span>
@@ -96,26 +110,6 @@
                                 <span>Create an account</span>
                             </a>
                         </p>
-
-                        <!-- Social -->
-                        <div class="my-6 divider">
-                            <div class="divider-text">or</div>
-                        </div>
-
-                        <div class="d-flex justify-content-center">
-                            <a href="javascript:;" class="btn btn-icon rounded-circle btn-text-facebook me-1_5">
-                                <i class="icon-base ti tabler-brand-facebook-filled icon-20px"></i>
-                            </a>
-                            <a href="javascript:;" class="btn btn-icon rounded-circle btn-text-twitter me-1_5">
-                                <i class="icon-base ti tabler-brand-twitter-filled icon-20px"></i>
-                            </a>
-                            <a href="javascript:;" class="btn btn-icon rounded-circle btn-text-github me-1_5">
-                                <i class="icon-base ti tabler-brand-github-filled icon-20px"></i>
-                            </a>
-                            <a href="javascript:;" class="btn btn-icon rounded-circle btn-text-google-plus">
-                                <i class="icon-base ti tabler-brand-google-filled icon-20px"></i>
-                            </a>
-                        </div>
                     </div>
                 </div>
                 <!-- /Login -->
