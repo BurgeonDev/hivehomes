cdd296de443a4790e541de7773bb42d98b8b9c2b
najjaf <najaf2961@gmail.com>
2025-08-26 20:11:18 +0500

    fix

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,27 +1,14 @@
- <footer class="content-footer footer bg-footer-theme">
-                        <div class="container-xxl">
-                            <div
-                                class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
-                                <div class="text-body">
-                                    ©
-                                    <script>
-                                        document.write(new Date().getFullYear());
-                                    </script>
-                                    , made with ❤️ by <a href="https://pixinvent.com" target="_blank"
-                                        class="footer-link">Pixinvent</a>
-                                </div>
-                                <div class="d-none d-lg-inline-block">
-                                    <a href="https://themeforest.net/licenses/standard" class="footer-link me-4"
-                                        target="_blank">License</a>
-                                    <a href="https://themeforest.net/user/pixinvent/portfolio" target="_blank"
-                                        class="footer-link me-4">More Themes</a>
-
-                                    <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/"
-                                        target="_blank" class="footer-link me-4">Documentation</a>
-
-                                    <a href="https://pixinvent.ticksy.com/" target="_blank"
-                                        class="footer-link d-none d-sm-inline-block">Support</a>
-                                </div>
-                            </div>
-                        </div>
-                    </footer>
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl">
+        <div class="py-4 footer-container d-flex align-items-center justify-content-between flex-md-row flex-column">
+            <div class="text-body">
+                ©
+                <script>
+                    document.write(new Date().getFullYear());
+                </script>
+                , Designed and Developed by
+                <a href="https://burgeon-grp.com" target="_blank" class="footer-link">Burgeon Group</a>
+            </div>
+        </div>
+    </div>
+</footer>
